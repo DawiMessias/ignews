3e18914dbe4ca2a53e51e5c401bb4ebeff181984
DawiMessias <wtih123@gmail.com>
2022-02-27 19:58:27 -0300

    Component subscribe

---
+++ src/components/SubscripeButton/index.tsx
@@ -0,0 +1,11 @@
+import styled from "./styled.module.scss"
+export function SubscriptButton() {
+    return (
+        <button 
+            type="button"
+            className={styled.subscribeButton}
+        >
+            Subscribe now
+        </button>
+    )
+}

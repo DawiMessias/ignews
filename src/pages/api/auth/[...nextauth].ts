63cead24d59563f225fd66374beeda2dab641ee8
DawiMessias <wtih123@gmail.com>
2022-03-20 20:52:36 -0300

    Indices para o FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,13 +17,29 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn({ user, account, profile }) { 
-      const { email } = user
+    const { email } = user
      try {
        await fauna.query(
-         q.Create(
-           q.Collection('users'),
-           { data:  { email }}
-         )
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data:  { email }}
+            ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+          )
        )
        return true;
      }catch (err){

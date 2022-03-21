2e470f6def0d0a0465a41556fdb71f9be06607b7
DawiMessias <wtih123@gmail.com>
2022-03-20 21:47:31 -0300

    Obtendo session do user via cookie

--- src/components/SubscripeButton/index.tsx
+++ src/components/SubscripeButton/index.tsx
@@ -1,9 +1,30 @@
+import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api"
 import styled from "./styled.module.scss"
 interface SubscribeButton {
     priceId: string;
 }
 
 export function SubscriptButton({priceId}: SubscribeButton) {
+    const {data: session } = useSession()
+
+    function handleSubscribe() {
+        if(!session) {
+            signIn("github")
+            return;
+        }
+
+        try {
+            const response = await api.post("/subscribe")
+            const { sessionId } = response.data
+           
+            
+        } catch () {
+
+        }
+
+        // criação da checkout session 
+    }
     return (
         <button 
             type="button"
